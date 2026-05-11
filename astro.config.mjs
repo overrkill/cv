ca1934cea422be1a36012d7647fce6e3f11b6419
Abhishek K <50524422+overrkill@users.noreply.github.com>
2026-05-12 00:36:27 +0530

    fix remove cloudflareext

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   integrations: [mdx()],
-  adapter: cloudflare(),
 });
